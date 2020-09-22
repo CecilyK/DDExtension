5ce9f67e9eb766dd85ff426c2402850f41c64ac3
CecilyK <cecily.kingori@strathmore.edu>
2020-09-22 09:52:18 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let body=document.querySelector("body")
+console.log("chrome extension running");
+const re=new RegExp('David Dobrik','gi')
+let matches=document.documentElement.innerHTML.match(re)
+//let paragraphs=matches.getElementsByTagName('span');
+//var myAudio = new Audio(chrome.runtime.getURL("song.mp3"));
+for(elt of matches){
+   elt.style['background-color']="#FF00FF";
+}/*
+ var myAudio = new Audio(chrome.runtime.getURL("song.mp3"));
+ console.log(matches);
+
+/*matches.addEventListener("onmouseover",function(){
+   myAudio.play();
+ });
+ for (let i = 0; i < matches.length; i++) {
+   matches[i].addEventListener("onmouseover",function(){
+      myAudio.play();
+    });
+}
+for (var i = 0; i < matches.length; i++) {
+   (function(index) {
+        matches[index].addEventListener("click", function() {
+           console.log("you clicked region number " + index);
+         })
+   })(i);
+}*/
