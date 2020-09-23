169454e47453b13ce3c66a26d33b108981667045
CecilyK <cecily.kingori@strathmore.edu>
2020-09-23 12:08:31 +0300

    TRIAL 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DDExtension
 A Chrome extension that plays the David Dobrik intro song when you hover over his name on any webpage.
+It also highlights his name when you hover over it.
--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 let body=document.querySelector("body")
+
 console.log("chrome extension running");
-const re=new RegExp('David Dobrik','gi')
+
+const re=new RegExp('David Dobrik','gi')//gi- case insensitive
+
 //let matches=document.documentElement.innerHTML.match(re)
-//let paragraphs=matches.getElementsByTagName('span');
+
 var myAudio = new Audio(chrome.runtime.getURL("song.mp3"));
+body.innerHTML=body.innerHTML.replace(re,"<span class='naughty' >$&</span>")//replace the words 'David Dobrik' with the span tag
 
 
-body.innerHTML=body.innerHTML.replace(re,"<span class='naughty'>$&</span>")
-
-
-
-document.getElementsByClassName("naughty").addEventListener("click",function(){
-   myAudio.play(); 
-});
-/*
- var myAudio = new Audio(chrome.runtime.getURL("song.mp3"));
- console.log(matches);
-
-/*matches.addEventListener("onmouseover",function(){
+document.addEventListener("click",function(){
    myAudio.play();
- });
- for (let i = 0; i < matches.length; i++) {
-   matches[i].addEventListener("onmouseover",function(){
-      myAudio.play();
-    });
-}
-for (var i = 0; i < matches.length; i++) {
-   (function(index) {
-        matches[index].addEventListener("click", function() {
-           console.log("you clicked region number " + index);
-         })
-   })(i);
-}*/
+});
+
